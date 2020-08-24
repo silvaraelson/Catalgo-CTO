94f4c986dbfd819fbe046964f653ce62c7f3fef0
silvaraelson <66041421+silvaraelson@users.noreply.github.com>
2020-08-23 22:41:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var botaoBuscar = document.getElementById('buscar')
+var area = document.getElementById("area")
+var grupo = getElementById("grupo")
+
+
+function teste(){
+
+    
+alert("www"+ Number( area.nodeValue) )
+   }
